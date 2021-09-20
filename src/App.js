584363eaed478b2008648a148101bb26f944ea38
Babu Rajendran <babu_rajendran@outlook.com>
2021-09-20 12:17:12 -0700

    Added image and updated the app to display list of dishes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurant from "./restaurant.jpg"
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>Welcome to my {props.adjective} kitchen!</p>
+      <img 
+        src={restaurant} 
+        alt="tables at a restaurant" 
+        height={200} />
+      <ul style = {{ textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}. All rights reserved.</p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Idli",
+  "Dosa",
+  "Upma",
+  "Pongal",
+  "Poori",
+  "Chappathi",
+  "Parotta",
+  "Masala Dosa",
+  "Uthappam"
+];
+
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish}))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Babu" />
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
